cd6f2f6086b3a7aa8da61356c3ee5dafed96e57f
Mitra <davoudabadi.mitra@gmail.com>
2021-07-05 20:44:59 -0600

    added the final styles

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -3,8 +3,13 @@ import { connect } from 'react-redux';
 import { startLogin } from '../actions/auth';
 
 export const LoginPage = ({ startLogin }) => (
-  <div>
-    <button onClick={startLogin}>Login</button>
+  <div className='box-layout' > 
+  <div className='box-layout__box'>
+    <h1 className='box-layout__title'>Expensify App</h1>
+    <p>Get Your Expenses Under Control!</p>
+  <button className='button' onClick={startLogin}>Login with Google</button>
+  </div>
+    
   </div>
 );
 
